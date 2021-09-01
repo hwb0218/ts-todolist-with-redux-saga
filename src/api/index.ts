cebeffae75ffd9156dd72d17593981fa4ffd5f67
WOOBEEN <hwb0218@gmail.com>
2021-09-02 05:50:11 +0900

    Feat: API 호출 함수 구현

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,14 +1,34 @@
-import { BASE_URL } from "utils/constant";
-import { ITodos } from "components/types";
+import { BASE_URL, URL_PATH, method, headers } from "utils/constant";
+import { ITodos, ITodo } from "types";
 
-export const fetchTodos = async (): Promise<ITodos> => {
-  const res = await fetch(`${BASE_URL}`);
+export interface IResponse {
+  status: number;
+  message: string;
+}
+
+const fetchTemplate = async (method: string, reqData?: any, id: number = 0) => {
+  const body = JSON.stringify(reqData);
+  const res = await fetch(`${BASE_URL}/${URL_PATH}/${id}`, { method, headers, body });
   const data = await res.json();
   return data;
 };
 
-// export const addTodos = async (): Promise<ITodos> => {
-//   // const res = await fetch("/data/data.json");
-//   // const data = await res.json();
-//   // return data;
-// };
+export const fetchTodos = async (): Promise<ITodos> => {
+  return fetchTemplate(method.get);
+};
+
+export const fetchAddTodo = async (): Promise<IResponse> => {
+  return fetchTemplate(method.post, { content: "string" }, 1);
+};
+
+export const fetchToggleTodoCheck = async (): Promise<IResponse> => {
+  return fetchTemplate(method.post, { isChecked: true }, 2);
+};
+
+export const fetchRemoveTodo = async (): Promise<IResponse> => {
+  return fetchTemplate(method.post, {}, 3);
+};
+
+export const fetchEditTodo = async (): Promise<IResponse> => {
+  return fetchTemplate(method.post, { content: "string" }, 4);
+};
