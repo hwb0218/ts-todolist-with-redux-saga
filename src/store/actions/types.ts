7fb19469ca332e6704a832f83e63dd5434f66a41
WOOBEEN <hwb0218@gmail.com>
2021-09-02 05:47:39 +0900

    Feat: Action 타입 선언

--- src/store/actions/types.ts
+++ src/store/actions/types.ts
@@ -1,7 +1,19 @@
 export const TODOS_FETCH_REQUEST = "todosFetchRequest" as const;
 export const TODOS_FETCH_SUCCEED = "todosFetchSucceed" as const;
 export const TODOS_FETCH_FAILED = "todosFetchFailed" as const;
-export const ADD_TODOS = "addTodos" as const;
+
+export const ADD_TODO_SUCCEED = "addTodoSucceed" as const;
+export const ADD_TODO_FAILED = "addTodoFailed" as const;
+
+export const TOGGLE_TODO_CHECK_SUCCEED = "toggleTodoCheckSucceed" as const;
+export const TOGGLE_TODO_CHECK_FAILED = "toggleTodoCheckFailed" as const;
+
+export const REMOVE_TODO_SUCCEED = "removeTodoSucceed" as const;
+export const REMOVE_TODO_FAILED = "removeTodoFailed" as const;
+
+export const EDIT_TODO_SUCCEED = "editTodoSucceed" as const;
+export const EDIT_TODO_FAILED = "editTodoFailed" as const;
+
 export const SHOW_MODAL = "showModal" as const;
 export const HIDE_MODAL = "hideModal" as const;
 export const TOGGLE_CIRCLE = "toggleCircle" as const;
